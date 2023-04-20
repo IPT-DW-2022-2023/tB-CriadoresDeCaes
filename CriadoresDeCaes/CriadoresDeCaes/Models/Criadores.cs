0d242675244c9754047f29a9d2dd7dbe4d9197be
jcnpereira <casimiro@ipt.pt>
2023-04-20 16:14:21 +0100

    adição de anotadores ao Model (Criadores)

--- CriadoresDeCaes/CriadoresDeCaes/Models/Criadores.cs
+++ CriadoresDeCaes/CriadoresDeCaes/Models/Criadores.cs
@@ -1,10 +1,18 @@
-﻿namespace CriadoresDeCaes.Models {
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CriadoresDeCaes.Models {
 
    /// <summary>
    /// descrição dos Criadores dos cães
    /// </summary>
    public class Criadores {
 
+      /*
+       * Lista de anotadores da classe 
+       * https://learn.microsoft.com/en-us/ef/ef6/modeling/code-first/data-annotations
+       * https://www.entityframeworktutorial.net/code-first/dataannotation-in-code-first.aspx
+       */
       public Criadores() {
          ListaAnimais=new HashSet<Animais>();
          ListaRacas=new HashSet<Racas>();
@@ -15,11 +23,13 @@
       /// <summary>
       /// Nome do criador
       /// </summary>
+      [Required(ErrorMessage ="O {0} é de preenchimento obrigatório")]
       public string Nome { get; set; }
 
       /// <summary>
       /// nome comercial do criador de cães
       /// </summary>
+      [Display(Name ="Nome Criador")]
       public string NomeCriador { get; set; }
 
       /// <summary>
@@ -30,16 +40,21 @@
       /// <summary>
       /// Código Postal
       /// </summary>
+      [DisplayName("Código Postal")]
       public string CodPostal { get; set; }
 
       /// <summary>
       /// email do criador
       /// </summary>
+      [EmailAddress(ErrorMessage = "O {0} não está corretamente escrito")]
+      [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
       public string Email { get; set; }
 
       /// <summary>
       /// telemóvel do criador
       /// </summary>
+      [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+      [Display(Name ="Telemóvel")]
       public string Telemovel { get; set; }
 
       /// <summary>
