5f733146b454d40e4cb2ede279bbe4fdef86d18e
jcnpereira <casimiro@ipt.pt>
2023-05-18 16:35:53 +0100

    actualização do controller animais

--- CriadoresDeCaes/CriadoresDeCaes/Controllers/AnimaisController.cs
+++ CriadoresDeCaes/CriadoresDeCaes/Controllers/AnimaisController.cs
@@ -13,7 +13,7 @@ namespace CriadoresDeCaes.Controllers {
       private readonly ApplicationDbContext _bd;
 
       public AnimaisController(ApplicationDbContext context) {
-         _bd=context;
+         _bd = context;
       }
 
       // GET: Animais
@@ -35,7 +35,7 @@ namespace CriadoresDeCaes.Controllers {
       // GET: Animais/Details/5
       public async Task<IActionResult> Details(int? id) {
 
-         if (id==null||_bd.Animais==null) {
+         if (id == null || _bd.Animais == null) {
             return NotFound();
          }
 
@@ -48,8 +48,8 @@ namespace CriadoresDeCaes.Controllers {
          var animal = await _bd.Animais
                                 .Include(a => a.Criador)
                                 .Include(a => a.Raca)
-                                .FirstOrDefaultAsync(m => m.Id==id);
-         if (animal==null) {
+                                .FirstOrDefaultAsync(m => m.Id == id);
+         if (animal == null) {
             return NotFound();
          }
 
@@ -65,8 +65,8 @@ namespace CriadoresDeCaes.Controllers {
       public IActionResult Create() {
 
          // prepara os dados a serem apresentados nas dropdown
-         ViewData["CriadorFK"]=new SelectList(_bd.Criadores, "Id", "Nome");
-         ViewData["RacaFK"]=new SelectList(_bd.Racas, "Id", "Nome");
+         ViewData["CriadorFK"] = new SelectList(_bd.Criadores, "Id", "Nome");
+         ViewData["RacaFK"] = new SelectList(_bd.Racas, "Id", "Nome");
 
          // invoca a View
          return View();
@@ -87,6 +87,65 @@ namespace CriadoresDeCaes.Controllers {
       [ValidateAntiForgeryToken]
       public async Task<IActionResult> Create([Bind("Id,Nome,DataNascimento,DataCompra,Sexo,NumLOP,RacaFK,CriadorFK")] Animais animal, IFormFile imagemAnimal) {
 
+         // avaliar se temos condições para tentar adicionar o animal
+         // testar se a Raça do animal != 0 e Criador =/= 0
+         if (animal.RacaFK == 0) {
+            // não foi escolhida uma raça
+            ModelState.AddModelError("", "É obrigatório escolher uma Raça.");
+         }
+         else {
+            // escolhi uma Raça. Mas, escolhi um Criador?
+            if (animal.CriadorFK == 0) {
+               // não escolhi Criador
+               ModelState.AddModelError("", "É obrigatório escolher o Criador do cão/cadela.");
+            }
+            else {
+               // se cheguei aqui, escolhi Raça e Criador
+               // será q escolhi Imagem? Vamos avaliar...
+
+               if (imagemAnimal == null) {
+                  // o utilizador não fez upload de uma imagem
+                  // vamos adicionar uma imagem prédefinida ao animal
+                  animal.ListaFotografias
+                        .Add(new Fotografias {
+                           Data = DateTime.Now,
+                           Local = "NoImage",
+                           NomeFicheiro = "noAnimal.jpg"
+                        });
+               }
+               else {
+                  // há ficheiro. Mas, será que é uma imagem?
+                  if (imagemAnimal.ContentType != "image/jpeg" ||
+                      imagemAnimal.ContentType != "image/png") {
+                     // o ficheiro carregado não é uma imagem
+                     // o que fazer?
+                     // Vamos fazer o mesmo que quando o utilizador não
+                     // fornece uma imagem
+                     animal.ListaFotografias
+                           .Add(new Fotografias {
+                              Data = DateTime.Now,
+                              Local = "NoImage",
+                              NomeFicheiro = "noAnimal.jpg"
+                           });
+                  }
+                  else {
+                     // há imagem!!!
+                     // determinar o nome da imagem
+                     Guid g = Guid.NewGuid();
+                     string nomeFoto=g.ToString();
+                     // obter a extensão do ficheiro
+                     string extensaoNomeFoto=Path.GetExtension(imagemAnimal.FileName).ToLower();
+                     nomeFoto += extensaoNomeFoto;
+
+
+
+
+                  }
+               }
+            }
+         }
+
+
          // se os dados recebidos respeitarem o modelo,
          // os dados podem ser adicionados
          if (ModelState.IsValid) {
@@ -101,15 +160,15 @@ namespace CriadoresDeCaes.Controllers {
             }
             catch (Exception) {
                ModelState.AddModelError("",
-                  "Ocorreu um erro com a adição dos dados do(a) "+animal.Nome);
-              // throw;
+                  "Ocorreu um erro com a adição dos dados do(a) " + animal.Nome);
+               // throw;
             }
 
          }
 
          // preparar estes dados, para quando os dados a introduzir na BD não estão bons
-         ViewData["CriadorFK"]=new SelectList(_bd.Criadores, "Id", "Nome", animal.CriadorFK);
-         ViewData["RacaFK"]=new SelectList(_bd.Racas, "Id", "Nome", animal.RacaFK);
+         ViewData["CriadorFK"] = new SelectList(_bd.Criadores, "Id", "Nome", animal.CriadorFK);
+         ViewData["RacaFK"] = new SelectList(_bd.Racas, "Id", "Nome", animal.RacaFK);
 
          // devolver à View os dados para correção
          return View(animal);
@@ -117,16 +176,16 @@ namespace CriadoresDeCaes.Controllers {
 
       // GET: Animais/Edit/5
       public async Task<IActionResult> Edit(int? id) {
-         if (id==null||_bd.Animais==null) {
+         if (id == null || _bd.Animais == null) {
             return NotFound();
          }
 
          var animais = await _bd.Animais.FindAsync(id);
-         if (animais==null) {
+         if (animais == null) {
             return NotFound();
          }
-         ViewData["CriadorFK"]=new SelectList(_bd.Criadores, "Id", "Email", animais.CriadorFK);
-         ViewData["RacaFK"]=new SelectList(_bd.Racas, "Id", "Id", animais.RacaFK);
+         ViewData["CriadorFK"] = new SelectList(_bd.Criadores, "Id", "Email", animais.CriadorFK);
+         ViewData["RacaFK"] = new SelectList(_bd.Racas, "Id", "Id", animais.RacaFK);
          return View(animais);
       }
 
@@ -136,7 +195,7 @@ namespace CriadoresDeCaes.Controllers {
       [HttpPost]
       [ValidateAntiForgeryToken]
       public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DataNascimento,DataCompra,Sexo,NumLOP,RacaFK,CriadorFK")] Animais animais) {
-         if (id!=animais.Id) {
+         if (id != animais.Id) {
             return NotFound();
          }
 
@@ -155,22 +214,22 @@ namespace CriadoresDeCaes.Controllers {
             }
             return RedirectToAction(nameof(Index));
          }
-         ViewData["CriadorFK"]=new SelectList(_bd.Criadores, "Id", "Email", animais.CriadorFK);
-         ViewData["RacaFK"]=new SelectList(_bd.Racas, "Id", "Id", animais.RacaFK);
+         ViewData["CriadorFK"] = new SelectList(_bd.Criadores, "Id", "Email", animais.CriadorFK);
+         ViewData["RacaFK"] = new SelectList(_bd.Racas, "Id", "Id", animais.RacaFK);
          return View(animais);
       }
 
       // GET: Animais/Delete/5
       public async Task<IActionResult> Delete(int? id) {
-         if (id==null||_bd.Animais==null) {
+         if (id == null || _bd.Animais == null) {
             return NotFound();
          }
 
          var animais = await _bd.Animais
              .Include(a => a.Criador)
              .Include(a => a.Raca)
-             .FirstOrDefaultAsync(m => m.Id==id);
-         if (animais==null) {
+             .FirstOrDefaultAsync(m => m.Id == id);
+         if (animais == null) {
             return NotFound();
          }
 
@@ -181,11 +240,11 @@ namespace CriadoresDeCaes.Controllers {
       [HttpPost, ActionName("Delete")]
       [ValidateAntiForgeryToken]
       public async Task<IActionResult> DeleteConfirmed(int id) {
-         if (_bd.Animais==null) {
+         if (_bd.Animais == null) {
             return Problem("Entity set 'ApplicationDbContext.Animais'  is null.");
          }
          var animais = await _bd.Animais.FindAsync(id);
-         if (animais!=null) {
+         if (animais != null) {
             _bd.Animais.Remove(animais);
          }
 
@@ -194,7 +253,7 @@ namespace CriadoresDeCaes.Controllers {
       }
 
       private bool AnimaisExists(int id) {
-         return _bd.Animais.Any(e => e.Id==id);
+         return _bd.Animais.Any(e => e.Id == id);
       }
    }
 }
