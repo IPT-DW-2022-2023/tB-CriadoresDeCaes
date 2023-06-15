37692a31dfeef366f556226a8b471486be83aa21
jcnpereira <casimiro@ipt.pt>
2023-06-15 16:41:43 +0100

    resolver não necessidade de dropdowns para escolher chaves forasteiras

--- CriadoresDeCaes/CriadoresDeCaes/Controllers/AnimaisController.cs
+++ CriadoresDeCaes/CriadoresDeCaes/Controllers/AnimaisController.cs
@@ -103,7 +103,7 @@ namespace CriadoresDeCaes.Controllers {
       public IActionResult Create() {
 
          // prepara os dados a serem apresentados nas dropdown
-         ViewData["CriadorFK"] = new SelectList(_bd.Criadores, "Id", "Nome");
+         //   ViewData["CriadorFK"] = new SelectList(_bd.Criadores, "Id", "Nome");
          ViewData["RacaFK"] = new SelectList(_bd.Racas, "Id", "Nome");
 
          // invoca a View
@@ -141,18 +141,28 @@ namespace CriadoresDeCaes.Controllers {
             ModelState.AddModelError("", "É obrigatório escolher uma Raça.");
          }
          else {
-            // escolhi uma Raça. Mas, escolhi um Criador?
-            if (animal.CriadorFK == 0) {
-               // não escolhi Criador
-               ModelState.AddModelError("", "É obrigatório escolher o Criador do cão/cadela.");
+            // se cheguei aqui, escolhi Raça
+            // será q escolhi Imagem? Vamos avaliar...
+
+            if (imagemAnimal == null) {
+               // o utilizador não fez upload de uma imagem
+               // vamos adicionar uma imagem prédefinida ao animal
+               animal.ListaFotografias
+                     .Add(new Fotografias {
+                        Data = DateTime.Now,
+                        Local = "NoImage",
+                        NomeFicheiro = "noAnimal.jpg"
+                     });
             }
             else {
-               // se cheguei aqui, escolhi Raça e Criador
-               // será q escolhi Imagem? Vamos avaliar...
-
-               if (imagemAnimal == null) {
-                  // o utilizador não fez upload de uma imagem
-                  // vamos adicionar uma imagem prédefinida ao animal
+               // há ficheiro. Mas, será que é uma imagem?
+               if (imagemAnimal.ContentType != "image/jpeg" &&
+                   imagemAnimal.ContentType != "image/png") {
+                  //  <=>  ! (imagemAnimal.ContentType == "image/jpeg" || imagemAnimal.ContentType == "image/png")
+                  // o ficheiro carregado não é uma imagem
+                  // o que fazer?
+                  // Vamos fazer o mesmo que quando o utilizador não
+                  // fornece uma imagem
                   animal.ListaFotografias
                         .Add(new Fotografias {
                            Data = DateTime.Now,
@@ -161,45 +171,28 @@ namespace CriadoresDeCaes.Controllers {
                         });
                }
                else {
-                  // há ficheiro. Mas, será que é uma imagem?
-                  if (imagemAnimal.ContentType != "image/jpeg" &&
-                      imagemAnimal.ContentType != "image/png") {
-                     //  <=>  ! (imagemAnimal.ContentType == "image/jpeg" || imagemAnimal.ContentType == "image/png")
-                     // o ficheiro carregado não é uma imagem
-                     // o que fazer?
-                     // Vamos fazer o mesmo que quando o utilizador não
-                     // fornece uma imagem
-                     animal.ListaFotografias
-                           .Add(new Fotografias {
-                              Data = DateTime.Now,
-                              Local = "NoImage",
-                              NomeFicheiro = "noAnimal.jpg"
-                           });
-                  }
-                  else {
-                     // há imagem!!!
-                     // determinar o nome da imagem
-                     Guid g = Guid.NewGuid();
-                     nomeFoto = g.ToString();
-                     // obter a extensão do ficheiro
-                     string extensaoNomeFoto = Path.GetExtension(imagemAnimal.FileName).ToLower();
-                     nomeFoto += extensaoNomeFoto;
+                  // há imagem!!!
+                  // determinar o nome da imagem
+                  Guid g = Guid.NewGuid();
+                  nomeFoto = g.ToString();
+                  // obter a extensão do ficheiro
+                  string extensaoNomeFoto = Path.GetExtension(imagemAnimal.FileName).ToLower();
+                  nomeFoto += extensaoNomeFoto;
 
-                     // guardar os dados do ficheiro na BD
-                     // para isso, vou associá-los ao 'animal'
-                     animal.ListaFotografias
-                           .Add(new Fotografias {
-                              Data = DateTime.Now,
-                              Local = "",
-                              NomeFicheiro = nomeFoto
-                           });
+                  // guardar os dados do ficheiro na BD
+                  // para isso, vou associá-los ao 'animal'
+                  animal.ListaFotografias
+                        .Add(new Fotografias {
+                           Data = DateTime.Now,
+                           Local = "",
+                           NomeFicheiro = nomeFoto
+                        });
 
-                     // informar a aplicação que há um ficheiro
-                     // (imagem) para guardar no disco rígido
-                     existeFoto = true;
-                  }
-               } // if (imagemAnimal == null)
-            } // if(animal.CriadoFK == 0)
+                  // informar a aplicação que há um ficheiro
+                  // (imagem) para guardar no disco rígido
+                  existeFoto = true;
+               }
+            } // if (imagemAnimal == null)
          } // if (animal.RacaFK == 0)
 
 
@@ -209,6 +202,17 @@ namespace CriadoresDeCaes.Controllers {
             animal.PrecoCompra = Convert.ToDecimal(animal.PrecoCompraAux.Replace('.', ','));
          }
 
+         // ********************************************************************
+         // atribuir o ID do Criador 
+         // ********************************************************************
+         // var auxiliar
+         string idDaPessoaAutenticada = _userManager.GetUserId(User);
+         int idCriador = await _bd.Criadores
+                                  .Where(c => c.UserId == idDaPessoaAutenticada)
+                                  .Select(c => c.Id)
+                                  .FirstOrDefaultAsync();
+         animal.CriadorFK = idCriador;
+         // ********************************************************************
 
          // se os dados recebidos respeitarem o modelo,
          // os dados podem ser adicionados
@@ -253,7 +257,7 @@ namespace CriadoresDeCaes.Controllers {
          }
 
          // preparar estes dados, para quando os dados a introduzir na BD não estão bons
-         ViewData["CriadorFK"] = new SelectList(_bd.Criadores, "Id", "Nome", animal.CriadorFK);
+         //        ViewData["CriadorFK"] = new SelectList(_bd.Criadores, "Id", "Nome", animal.CriadorFK);
          ViewData["RacaFK"] = new SelectList(_bd.Racas, "Id", "Nome", animal.RacaFK);
 
          // devolver à View os dados para correção
